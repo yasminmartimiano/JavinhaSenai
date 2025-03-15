fa6c0b8eb519e29bae36801e8663d2fabe5cfc33
yasminmartimiano <yasminliver52@gmail.com>
2025-03-14 21:34:33 -0300

    Exercício 02

--- Construtores/MainPessoa.java
+++ Construtores/MainPessoa.java
@@ -1,4 +1,13 @@
 package Construtores;
 
 public class MainPessoa {
+    public static void main(String[] args) {
+        Pessoa pessoaConhecida = new Pessoa("Manoel Gomes",55);
+        Pessoa pessoaDesconhecida = new Pessoa();
+
+        System.out.println("Pessoa conhecida:" );
+        pessoaConhecida.exibirDados();
+        System.out.println("Pessoa desconhecida:" );
+        pessoaDesconhecida.exibirDados();
+    }
 }
--- Construtores/Pessoa.java
+++ Construtores/Pessoa.java
@@ -1,4 +1,21 @@
 package Construtores;
 
 public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void exibirDados(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+
+    public Pessoa(){
+        this.nome = "Desconhecido";
+        this.idade = 0;
+    }
 }
