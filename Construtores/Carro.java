c2185e6167522d302f8b0dc712b16b79a546ee29
yasminmartimiano <yasminliver52@gmail.com>
2025-03-14 21:03:05 -0300

    Exercício 01

--- Construtores/Carro.java
+++ Construtores/Carro.java
@@ -1,4 +1,15 @@
 package Construtores;
 
 public class Carro {
+    private String marca;
+    private int ano;
+
+    Carro(String marca, int ano){
+        this.marca = marca;
+        this.ano = ano;
+    }
+
+    public void mostrarInformacoes() {
+        System.out.println("Marca: " + marca + "\nAno: " + ano);
+    }
 }
--- Construtores/MainCarro.java
+++ Construtores/MainCarro.java
@@ -1,4 +1,8 @@
 package Construtores;
 
 public class MainCarro {
+    public static void main(String[] args) {
+        Carro carro = new Carro("Bentley" , 2003);
+        carro.mostrarInformacoes();
+    }
 }
